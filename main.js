5bc3dc06c5eba01845172ab3d46827027146fb40
Mangondato9Say2526 <mangondato.ash@obmontessori.edu.ph>
2026-02-10 08:54:08 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+ function result(){
+    let radius = document.getElementById("num1").value;
+    let result = Math.PI * radius * radius;
+    document.getElementById("num2").value = result;        
+    }
